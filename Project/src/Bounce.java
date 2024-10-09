a21d5cee1c1521eefe7b579e9141b4d944f99bbf
Ashutosh Ayush <jbnashu297@gmail.com>
2024-10-09 19:59:09 +0530

    Java panel

--- Project/src/Bounce.java
+++ Project/src/Bounce.java
@@ -46,3 +46,15 @@ class Ball {
     private double dx = 1;
     private double dy = 1;
 }
+class BallPanel extends JPanel{
+    public void add(Ball b){
+        balls.add(b);
+    }
+    public void paintComponent(Graphics g){
+        super.paintComponent(g);
+        Graphics2D g2=(Graphics2D)g;
+        for(Ball b:balls){
+            g2.fill(b.getShape());
+        }
+    }
+}
